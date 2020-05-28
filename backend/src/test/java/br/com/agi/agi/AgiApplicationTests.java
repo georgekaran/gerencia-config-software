ff28a8c86c0564b0780c153c402480d41afe5d65
georgekaran <georgekaran12@gmail.com>
2020-05-27 21:13:55 -0300

    Testing pipeline Jenkins

--- backend/src/test/java/br/com/agi/agi/AgiApplicationTests.java
+++ backend/src/test/java/br/com/agi/agi/AgiApplicationTests.java
@@ -4,12 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@TestPropertySource(
+		locations = "classpath:application-integrationtest.properties")
 public class AgiApplicationTests {
 
 	@Autowired
