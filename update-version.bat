09d267eb6102981fdf1d2d30d02aeab41038508d
georgekaran <georgekaran12@gmail.com>
2020-06-17 20:49:40 -0300

    v 1.5

--- update-version.bat
+++ update-version.bat
@@ -4,7 +4,7 @@ SET home=%cd%
 
 :: BACKEND
 cd %home%\backend\
-mvn versions:set -DnewVersion=%1 -DgenerateBackupPoms=false -DprocessAllModules=true
+mvnw versions:set -DnewVersion=%1 -DgenerateBackupPoms=false -DprocessAllModules=true
 
 :: FRONT-END
 cd %home%\app\
